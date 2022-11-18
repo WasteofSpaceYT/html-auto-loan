f6fc5e40bf0996e1e3cfa1e20a3dad10b87197cb
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-11-18 09:47:54 -0500

    Added tax to calculation

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+// Calculates the loan amount and monthly payments
 function calculate() {
+    // Get the values from the input fields
     amnt = El("loanAmount").value
+    tax = El("tax").value
     term = El("loanTerm").value
     rate = El("interestRate").value
 
+    // Show information about the loan
     El("calcAmnt").textContent = "$" + parseInt(amnt).toLocaleString("en-US")
+    El("calcTax").textContent = "$" + ((tax/100)*amnt).toLocaleString()
     El("calcTerm").textContent = term + " months"
     El("calcRate").textContent = rate + "%"
-    El("monthly").textContent = "$" + ((amnt*(1+rate/12)^(12*(term/12)))/term).toLocaleString("en-US")
+
+    // p*(1/n)^rt
+    El("monthly").textContent = "$" + (((amnt*(1+rate/12)^(12*(term/12)))/term)+((tax/100)*amnt)).toLocaleString("en-US")
 
     El("loandat").hidden = false;
 }
 
+// QOL function to get elements by ID
 function El(name) {
     return document.getElementById(name);
 }
